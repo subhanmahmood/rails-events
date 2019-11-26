4032bb385adc7e04ae4ef0a49d66893b4592cef2
Subhan Mahmood <subhan70m@gmail.com>
2019-11-26 16:56:01 +0000

    added events scaffold

---
+++ db/migrate/20191126165136_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :location
+      t.date :date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_235756) do
+ActiveRecord::Schema.define(version: 2019_11_26_165136) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "location"
+    t.date "date"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
