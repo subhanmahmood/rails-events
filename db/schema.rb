e964b50342ba0d5ed45c83f7a5b8a4a71ae7c4aa
Subhan Mahmood <subhan70m@gmail.com>
2019-11-26 18:00:47 +0000

    fixed login form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_165136) do
+ActiveRecord::Schema.define(version: 2019_11_26_165626) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_165136) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_events_on_title", unique: true
   end
 
   create_table "users", force: :cascade do |t|
