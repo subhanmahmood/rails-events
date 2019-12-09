144c23426ffeb2edbaf7d06c432e2386a894702f
Subhan Mahmood <subhan70m@gmail.com>
2019-12-09 17:05:30 +0000

    .

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -7,22 +7,23 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @event = events(:one)
     @user = users(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
-    sign_in users(:one)
+    
     get events_url
     assert_response :success
   end
 
   test "should get new" do
-    sign_in users(:one)
+    
     get new_event_url
     assert_response :success
   end
 
   test "should create event" do
-    sign_in users(:one)
+    
     assert_difference('Event.count') do
       post events_url, params: { event: { date: @event.date, description: @event.description, location: @event.location, title: @event.title, user_id: @event.user_id } }
     end
@@ -31,25 +32,25 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show event" do
-    sign_in users(:one)
+    
     get event_url(@event)
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in users(:one)
+    
     get edit_event_url(@event)
     assert_response :success
   end
 
   test "should update event" do
-    sign_in users(:one)
+    
     patch event_url(@event), params: { event: { date: @event.date, description: @event.description, location: @event.location, title: @event.title, user_id: @event.user_id } }
     assert_redirected_to event_url(@event)
   end
 
   test "should destroy event" do
-    sign_in users(:one)
+    
     assert_difference('Event.count', -1) do
       delete event_url(@event)
     end
