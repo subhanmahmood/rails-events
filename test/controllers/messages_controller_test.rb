41fcce2d8c49344cc96164facfdc2465e8625bdb
Subhan Mahmood <subhan70m@gmail.com>
2019-12-14 21:09:40 +0000

    removed unnecessary message controller tests

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -8,33 +8,12 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
     @message = messages(:one)
     sign_in users(:one)
   end
-
-  test "should get index" do
-    get messages_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_message_url(event_id: @event)
-    assert_response :success
-  end
-
   test "should create message" do
     assert_difference('Message.count') do
       post messages_url, params: { message: { body: @message.body, event_id: @message.event_id, user_id: @message.user_id } }
     end
 
     assert_redirected_to message_url(Message.last)
-  end
-
-  test "should show message" do
-    get message_url(@message)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_message_url(@message)
-    assert_response :success
   end
 
   test "should update message" do
